74180093f1d53a163688bc488f1d39705a42fd79
avysel <avanryssel@gmail.com>
2017-11-05 16:58:57 +0100

    init

--- src/main/java/com/avysel/blockchain/Main.java
+++ src/main/java/com/avysel/blockchain/Main.java
@@ -1,7 +1,12 @@
 package com.avysel.blockchain;
 
+import com.avysel.blockchain.business.Chain;
+import com.avysel.blockchain.business.ChainManager;
+
 public class Main {
 	public static void main (String[] args) {
 
+		Chain chain = new Chain();
+		ChainManager manager = new ChainManager(chain);
 	}
 }
--- src/main/java/com/avysel/blockchain/business/ChainManager.java
+++ src/main/java/com/avysel/blockchain/business/ChainManager.java
@@ -3,6 +3,14 @@ package com.avysel.blockchain.business;
 public class ChainManager {
 	private Chain chain;
 
+	public ChainManager() {
+		
+	}
+	
+	public ChainManager(Chain chain) {
+		this.chain = chain;
+	}
+	
 	public Chain getChain() {
 		return chain;
 	}

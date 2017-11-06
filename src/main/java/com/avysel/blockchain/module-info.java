f1f2caf253a59974fdc07ece1b6c57f40368ad1e
Alexandre VANRYSSEL <avanryssel@banque-accord.com>
2017-11-06 14:31:58 +0100

    module info

---
+++ src/main/java/com/avysel/blockchain/module-info.java
@@ -0,0 +1,6 @@
+module com.avysel.blockchain {
+	exports com.avysel.blockchain.business;
+	exports com.avysel.blockchain.crypto;
+	exports com.avysel.blockchain.model;
+	exports com.avysel.blockchain.network;
+}

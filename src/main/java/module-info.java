e1c035cb88e6ae37c4f23def355a964609ac3945
Alexandre Vanryssel <avanryssel@gmail.com>
2017-11-22 15:35:01 +0100

    module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module com.avysel.blockchain {
+	exports com.avysel.blockchain.business;
+	
+	exports com.avysel.blockchain.crypto
+	to com.avysel.blockchain;
+	
+	exports com.avysel.blockchain.model.block
+	to com.avysel.blockchain;
+	
+	exports com.avysel.blockchain.model.chain
+	to com.avysel.blockchain;
+	
+	exports com.avysel.blockchain.model.data
+	to com.avysel.blockchain;
+	
+	exports com.avysel.blockchain.network
+	to com.avysel.blockchain;
+	
+	exports com.avysel.blockchain.db
+	to com.avysel.blockchain;
+}
